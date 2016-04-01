1b8f0e2f6cb0dd876bec4803f59566ab9031f95b
imotai <codego.me@gmail.com>
2016-04-01 18:03:26 +0800

    update stop script

--- sandbox/stop_all.sh
+++ sandbox/stop_all.sh
@@ -1,3 +1,1 @@
-killall sloth 
-ps -ef | grep dqs | grep -v grep | awk '{print $2}' | while read line; do kill -9 $line;done
-ps -ef | grep perf | grep -v grep | awk '{print $2}' | while read line; do kill -9 $line;done
+ps -ef | grep sloth | grep -v grep | awk '{print $2}' | while read line; do kill -9 $line;done
