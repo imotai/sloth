0bfde3f29dab7f71552640a9525c0f8c332ea849
wangtaize <codego.me@gmail.com>
2016-04-06 23:29:30 +0800

    add event

--- src/sloth_core.h
+++ src/sloth_core.h
@@ -1,5 +1,6 @@
 #ifndef SLOTH_CORE_H
 #define SLOTH_CORE_H
+#include <boost/lockfree/queue.hpp>
 #include "proto/sloth_node.pb.h"
 
 using ::google::protobuf::RpcController;
@@ -7,12 +8,23 @@ using ::google::protobuf::Closure;
 
 namespace sloth {
 
+enum SlothEventType {
+  kAppendEntry = 0;
+  kElectionTimeout = 1;
+  kVoteTimeout = 2;
+};
+
+struct SlothEvent {
+  void* data;
+  SlothEventType type;
+};
+
 // the core logic for raft 
 // all functions will be processed by one thread and no mutex lock
 class SlothCore {
 
 public:
-  SlothCore();
+  SlothCore(boost::lockfree::queue<SlothEvent>* queue);
   ~SlothCore();
   void AppendEntries(const AppendEntriesRequest* request,
                      AppendEntriesResponse* response,
@@ -21,6 +33,7 @@ public:
 private:
   uint64_t current_term_;
   SlothNodeRole role_;
+  boost::lockfree::queue<SlothEvent>* queue_;
 };
 
 }
